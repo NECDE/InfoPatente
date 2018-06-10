1bd7242cdf4314ef51e4874aa579aa6a3a790627
León Sepúlveda <NECDE@users.noreply.github.com>
2018-06-10 13:56:54 -0400

    Add files via upload

--- patentes.py
+++ patentes.py
@@ -1,64 +1,14 @@
-#!/usr/bin/python
-#-*- coding:UTF-8 -*-
-
 from bs4 import BeautifulSoup
 import requests
 import http.client
 import urllib
 import json
 from imagesoup import ImageSoup
+import sys
 
-file = open('InfoPatente.txt','w')
-
-def multas_total(patente):
-	link       = "http://consultamultas.srcei.cl/ConsultaMultas/buscarConsultaMultasExterna.do"
-	host       = "consultamultas.srcei.cl"
-	parametros = 'ppu=%s' % patente
-	headers    = {'Content-Type':'application/x-www-form-urlencoded','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0','Cookie':'JSESSIONID=32F21D2C90883C05DEA350CBA98E1CDD'}
-	conexionmultas   = http.client.HTTPConnection(host)
-
-	conexionmultas.request("POST", link, parametros, headers)
-	request    = conexionmultas.getresponse()
-	respuesta  = request.read()
-	conexionmultas.close()
-	html       = BeautifulSoup(respuesta,"html5lib")
-	file.write(html)
-	entradas   = html.find_all('div', {'class':'textEncabezadoTablaVentas'})
-	final      = []
-
-	# SACANDO MULTAS DE LA PAGINA 
-	def has_six_characters(css_class):
-		return css_class is not None and len(css_class) == 9
-
-	multass = html.find_all('td',class_=has_six_characters)
-	doblemultas = len(multass)
-	totalmultas = doblemultas // 2
-	contador=0
-	paragraphs = []
-	for x in multass:
-		paragraphs.append(str(x))
-
-	for i in paragraphs:
-		print(i[22:-5])
-		file.write('\n'+i[22:-5])
-		contador+=1
-		if contador == totalmultas:
-			break
-		else:
-			pass
-
-	for item in entradas:
-		item.encode('utf-8')
-		resultado  = item.getText()
-		final.append(resultado)
-
-	if len(final) > 0:
-		return final[0]
-	else:
-		file.write("Sin multas")
-		return ("Sin multas")
 
 def tipoVehiculo(numero):
+
 	if numero == "1":
 		tipo = 'AUTOMOVIL'
 	elif numero == "2":
@@ -78,7 +28,120 @@ def tipoVehiculo(numero):
 
 	return tipo
 
+def imagenes_google(marca,modelo,ano):
+	# BUSQUEDA DE IMAGENES DEL MODELO DEL AURO EN GOOGLE
+
+	busqueda = marca+modelo+ano
+
+	if len(busqueda) == 1:
+		print("\nError consiguiendo datos de SOAP")
+	elif len(sys.argv) >= 3:
+		try:
+			auto = ImageSoup()
+			images = auto.search(busqueda, n_images=5)
+			firstimage = images[0]
+			secondimage = images[1]
+			thirdimage = images[2]
+			firstimage.show()
+			secondimage.show()
+			thirdimage.show()
+		except:
+			print("Error en busqueda de imagen de vehiculo")
+	else:
+		pass
+
+def datos_persona_auto():
+
+	try:
+		url = "https://soap.uanbai.com/bci/soap/2018/ajax/loadPPU.jsp?PPU=%s&SES=DDB9674E703F9BB04C4F3BB2D96D8291.worker1" % patente
+		peticion = requests.get(url)
+		datos    = json.loads(peticion.content)
+
+	except:
+		print ("\nNo se pudo obtener la información (SOAP)")
+
+	# Datos del dueño
+	
+	nombredueno = (" Nombre: " + datos['propietario']['nombre'] + " " + datos['propietario']['ap_paterno'] + " " + datos['propietario']['ap_materno'])
+	rutdueno = (" RUT: " + datos['propietario']['rut'] +"-" + datos['propietario']['dv'])
+	
+	print ("\n+--------- Datos del Dueño -------+")
+	print(nombredueno)
+	print(rutdueno)
+
+	# Datos del vehiculo
+	
+	tipovehi = (" Tipo: " + tipoVehiculo(datos['id_tipo']))
+	marcavehi = (" Marca: " + datos['marca'])
+	modelovehi = (" Modelo: "+ datos['modelo'])
+	anoauto = (" Año: " + str(datos['ano']))
+	nmotor = (" N° Motor: " + str(datos['vin']))
+	dvpatente = (" DV patente: " + str(datos['dvpatente']))
+
+	print("\n+--------- Datos del Vehiculo -------+")
+	print(tipovehi+"\n"+marcavehi+"\n"+modelovehi+"\n"+anoauto+"\n"+nmotor+"\n"+dvpatente)
+
+	# Imagenes del auto en google
+
+	if len(sys.argv) >= 3 and sys.argv[3] == "-i" or "images":
+		imagenes_google(datos['marca'],datos['modelo'],datos['ano'])
+	else:
+		print("\nNo mostrando imagenes")
+
+def multas_total(patente):
+
+	link       = "http://consultamultas.srcei.cl/ConsultaMultas/buscarConsultaMultasExterna.do"
+	host       = "consultamultas.srcei.cl"
+	parametros = 'ppu=%s' % patente
+	headers    = {'Content-Type':'application/x-www-form-urlencoded','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0','Cookie':'JSESSIONID=32F21D2C90883C05DEA350CBA98E1CDD'}
+	conexionmultas   = http.client.HTTPConnection(host)
+
+	conexionmultas.request("POST", link, parametros, headers)
+	request    = conexionmultas.getresponse()
+	respuesta  = request.read()
+	conexionmultas.close()
+	html       = BeautifulSoup(respuesta,"html5lib")
+	entradas   = html.find_all('div', {'class':'textEncabezadoTablaVentas'})
+	final      = []
+
+	# SACANDO MULTAS DE LA PAGINA 
+	def has_six_characters(css_class):
+		return css_class is not None and len(css_class) == 9
+
+	multass = html.find_all('td',class_=has_six_characters)
+	doblemultas = len(multass)
+	totalmultas = doblemultas // 2
+	contador=0
+	paragraphs = []
+	for x in multass:
+		paragraphs.append(str(x))
+
+	for i in paragraphs:
+		print(i[22:-5])
+		contador+=1
+		if contador == totalmultas:
+			break
+		else:
+			pass
+
+	for item in entradas:
+		item.encode('utf-8')
+		resultado  = item.getText()
+		final.append(resultado)
+
+	if len(final) > 0:
+		return final[0]
+	else:
+		print ("Sin multas")
+
+	try:
+		multastotales = str(multas_total(patente))
+		print (" Total multas: " + multastotales)
+	except:
+		print(" Error en acceso a datos de ServRegistroCivil")
+
 def transportePublico(patente):
+
 	urlPublico      = "http://apps.mtt.cl/consultaweb/default.aspx"
 	peticionPublico = requests.get(urlPublico)
 	htmlPublico     = BeautifulSoup(peticionPublico.content ,"html5lib")
@@ -93,82 +156,12 @@ def transportePublico(patente):
 	request   = conexiontranspublic.getresponse()
 	respuesta = request.read()
 	conexiontranspublic.close()
-	return respuesta
 
-def runprogram():
-
-	print ('''
-	╔═══╗╔═══╗╔════╗╔═══╗╔═╗ ╔╗╔════╗╔═══╗╔═══╗
-	║╔═╗║║╔═╗║║╔╗╔╗║║╔══╝║║╚╗║║║╔╗╔╗║║╔══╝║╔═╗║
-	║╚═╝║║║ ║║╚╝║║╚╝║╚══╗║╔╗╚╝║╚╝║║╚╝║╚══╗║╚══╗
-	║╔══╝║╚═╝║  ║║  ║╔══╝║║╚╗║║  ║║  ║╔══╝╚══╗║
-	║║   ║╔═╗║  ║║  ║╚══╗║║ ║║║  ║║  ║╚══╗║╚═╝║
-	╚╝   ╚╝ ╚╝  ╚╝  ╚═══╝╚╝ ╚═╝  ╚╝  ╚═══╝╚═══╝
-				by @UnknDown
-				 modded by NECDE
-	''')
-	patente = input("Ingrese Patente a consultar: ")
-	print("\nPATENTE: " , patente.upper())
-	file.write("\nPATENTE: "+patente.upper())
-	try:
-		url = "https://soap.uanbai.com/bci/soap/2018/ajax/loadPPU.jsp?PPU=%s&SES=DDB9674E703F9BB04C4F3BB2D96D8291.worker1" % patente
-		peticion = requests.get(url)
-		datos    = json.loads(peticion.content)
-
-	except:
-		print ("No se pudo obtener la información (SOAP)")
-		file.write("No se pudo obtener la información (SOAP)")
-		exit()
-
-	# Datos del dueño
-	
-	nombredueño = (" Nombre: " + datos['propietario']['nombre'] + " " + datos['propietario']['ap_paterno'] + " " + datos['propietario']['ap_materno'])
-	rutdueño = (" RUT: " + datos['propietario']['rut'] +"-" + datos['propietario']['dv'])
-	
-	print ("\n+--------- Datos del Dueño -------+")
-	print(nombredueño)
-	print(rutdueño)
-
-	file.write ("\n\n+--------- Datos del Dueño -------+\n")
-	file.write (nombredueño)
-	file.write (rutdueño)
-
-	# Datos del vehiculo
-	
-	tipovehi = (" Tipo: " + tipoVehiculo(datos['id_tipo']))
-	marcavehi = (" Marca: " + datos['marca'])
-	modelovehi = (" Modelo: "+ datos['modelo'])
-	añovehi = (" Año: " + str(datos['ano']))
-	nmotor = (" N° Motor: " + str(datos['vin']))
-	dvpatente = (" DV patente: " + str(datos['dvpatente']))
-
-	print("\n+--------- Datos del Vehiculo -------+")
-	print(tipovehi+"\n"+marcavehi+"\n"+modelovehi+"\n"+añovehi+"\n"+nmotor+"\n"+dvpatente)
-
-	file.write("\n\n+--------- Datos del Vehiculo -------+\n")
-	file.write(tipovehi+"\n"+marcavehi+"\n"+modelovehi+"\n"+añovehi+"\n"+nmotor+"\n"+dvpatente)
-
-	# Datos legales del vehiculo
-
-	print ("\n+--------- Datos Legales -------+")
-	file.write ("\n\n+--------- Datos Legales -------+\n")
-
-	try:
-		multastotales = str(multas_total(patente))
-		print (" Total multas: " + multastotales)
-		file.write ("\n\n Total multas: " + multastotales)
-	except:
-		print(" Error en acceso a datos de ServRegistroCivil")
-		file.write("\nError en acceso a datos de ServRegistroCivil")
-
-	# Verificamos si pertence al transporte publico
-	publico = transportePublico(patente)
 	print ("\n+--------- Datos transporte publico -------+")
-	file.write("\n\n+--------- Datos transporte publico -------+")
 
 	label    = []
 	content  = []
-	html     = BeautifulSoup(publico,"html5lib")
+	html     = BeautifulSoup(respuesta,"html5lib")
 	entradas = html.find_all('td', {'class':'label'})
 	cont     = html.find_all('span')
 
@@ -190,35 +183,50 @@ def runprogram():
 	if total_label > 0:
 		for x in range(0,total_label):
 			print (" "+label[x]+": " + content[x])
-			file.write(" "+label[x]+": " + content[x])
 
 		if content[total_content-2] != "":
 			print (" Conductores: " + content[total_content-2])
-			file.write(" Conductores: " + content[total_content-2])
 		else:
 			print (" Conductores: -")
-			file.write(" Conductores: -")
 	else:
 		print (" El vehiculo no pertenece al transporte publico")
-		file.write("\n\n El vehiculo no pertenece al transporte publico")
-	file.close() 
 
+def runprogram():
+	print ('''\n\n
+	╔═══╗╔═══╗╔════╗╔═══╗╔═╗ ╔╗╔════╗╔═══╗╔═══╗
+	║╔═╗║║╔═╗║║╔╗╔╗║║╔══╝║║╚╗║║║╔╗╔╗║║╔══╝║╔═╗║
+	║╚═╝║║║ ║║╚╝║║╚╝║╚══╗║╔╗╚╝║╚╝║║╚╝║╚══╗║╚══╗
+	║╔══╝║╚═╝║  ║║  ║╔══╝║║╚╗║║  ║║  ║╔══╝╚══╗║
+	║║   ║╔═╗║  ║║  ║╚══╗║║ ║║║  ║║  ║╚══╗║╚═╝║
+	╚╝   ╚╝ ╚╝  ╚╝  ╚═══╝╚╝ ╚═╝  ╚╝  ╚═══╝╚═══╝
+				by @UnknDown
+				 modded by NECDE
+	''')
+	
+	print("\nPATENTE: " , patente.upper())
 
-	# Buscar-Descargar-Mostrar imagenes del vehiculo (google images)
-	busqueda = datos['marca']+" "+datos['modelo']
-	if len(busqueda) == 1:
-		print("\nError consiguiendo datos de SOAP")
-	else:
-		try:
-			auto = ImageSoup()
-			images = auto.search(busqueda, n_images=5)
-			firstimage = images[0]
-			secondimage = images[1]
-			thirdimage = images[2]
-			firstimage.show()
-			secondimage.show()
-			thirdimage.show()
-		except:
-			print("Error en busqueda de imagen de vehiculo")
+	# DATOS PERSONA Y AUTO
+	try:
+		datos_persona_auto()
+	except:
+		print(" Error obteniendo datos personales y del auto")
 
-runprogram()
+	# DATOS LEGALES
+	print ("\n+--------- Datos Legales -------+")
+	try:
+		multas_total(patente)
+	except:
+		print(" Error obteniendo multas")
+
+	# DATOS SI ES TRANSPORTE PUBLICO
+	transportePublico(patente)
+
+if len(sys.argv) >= 2:
+	patente = sys.argv[1]
+else:
+	print("""Usage: python patentes.py aabb11 -i
+""")
+if len(sys.argv) >= 2 and len(sys.argv[1]) == 6:
+	runprogram()
+else:
+	print("\n Ingresa una patente con 6 valores alfanumericos\n")
